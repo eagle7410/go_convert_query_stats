5657b132d2f10e16f9edb0df1d1fa2ab97651f2c
Igor Stcherbina <orel1939@mail.ru>
2016-07-03 00:05:12 +0300

    Have async job in collcted

--- convert_query_stats.go
+++ convert_query_stats.go
@@ -3,23 +3,18 @@ package main
 import (
 	"./db/contextLogs"
 	"./utils/json"
-	//"labix.org/v2/mgo/bson"
-
 	"runtime"
-	//"strconv"
+	"strconv"
 	"time"
-	//"sync"
+	"sync"
 	"fmt"
 
 	//"gopkg.in/mgo.v2/bson"
-	//"github.com/davecgh/go-spew/spew"
+//	"github.com/davecgh/go-spew/spew"
 )
 
-const steep  = 100000
+const steep  = 20000
 type (
-	Count struct {
-		val int
-	}
 	StatsDay  map[string]int
 	StatsDays map[string]StatsDay
 	Stats     struct {
@@ -31,7 +26,7 @@ type (
 
 var (
 	start   time.Time
-	Queris  TQueris
+	wg      sync.WaitGroup
 	//mu      sync.Mutex
 	//run     int
 	//end     int
@@ -39,33 +34,38 @@ var (
 
 func main()  {
 	start  = time.Now()
-	Queris = make(TQueris)
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	queryCollected ()
+	runtime.GOMAXPROCS(runtime.NumCPU()*2)
+	max, err := contextLogs.Count()
 
+	if err != nil {
+		fmt.Println("Error get Count", err)
+		return
+	}
+
+	max = 1000
+
+	for i:=0; i < max;  {
+		wg.Add(1)
+		go queryCollected (i)
+		i +=steep
+	}
+
+	wg.Wait()
+
+	fmt.Println("the end", time.Since(start))
 }
 
-//func AddQuery(query string, wgPoint *sync.WaitGroup) {
-//func AddQuery(query string) {
-//	//Queris = append(queris, query)
-//}
-//func UnlockQueris (wgPoint *sync.WaitGroup) {
-//
-//	fmt.Println("End tread", run, end)
-//	wgPoint.Done()
-//}
-
-
-func queryCollected () {
+func queryCollected (skip int) {
 	//p := contextLogs.Params{ Limit: steep, Query: bson.M{"geo.country" : bson.M{ "$exists" : true}}}
-	p := contextLogs.Params{ Limit: steep}
+	Queris := make(TQueris)
+	p := contextLogs.Params{ Limit: steep, Skip: skip }
 
 	arContextLogs := contextLogs.All(p)
 
 	//for i,log := range arContextLogs {
 	for _,log := range arContextLogs {
 
-		//contextLogs.Print(log);
+//		contextLogs.Print(log);
 		country := "ALL";
 
 		if len(log.Geo) != 0 {
@@ -105,29 +105,36 @@ func queryCollected () {
 		if country != "ALL" {
 			query.Data[stDate][country]++
 		}
+//		if log.Uid !=  {
+//
+//		}
+
+//		spew.Dump(statCountry)
+//
 
 		query.Count++
 
-		//if i % 1000 == 0 {
-		//	fmt.Println("__ITER ", i)
-		//}
-		//fmt.Println("__ITER ", i)
 	}
 
-	_, err := json.ToFile(Queris, "./buffer/res")
+	strSkip := strconv.Itoa(skip)
+	_, err := json.ToFile(Queris, "./buffer/res_" + strSkip )
 
 	if err != nil {
 		fmt.Println("Error write to json ", err)
 	}
 
-	Queris2 := make(TQueris)
+	fmt.Println("Skip " + strSkip + " END")
 
-	json.FromFile("./buffer/res", &Queris2)
+	defer wg.Done()
 
-	fmt.Println("the end", time.Since(start), len(Queris), len(Queris2))
-	//spew.Dump(Queris2)
+//	Queris2 := make(TQueris)
+
+//	json.FromFile("./buffer/res", &Queris2)
+
+
+//	spew.Dump(Queris)
 }
 
-//	var wg sync.WaitGroup
+//	var
 //	wg.Add(len(arContextLogs))
-//	wg.Wait()
+//
